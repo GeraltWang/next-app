664cbb09ee8857a20f678b11ab65e0160d6504fd
GeraldWang <wgeralt@outlook.com>
2023-12-17 16:36:21 +0800

    feat: ✨ setup image uploader

---
+++ app/upload/page.tsx
@@ -0,0 +1,40 @@
+'use client'
+import React, { useState } from 'react'
+import { CldUploadWidget, CldImage } from 'next-cloudinary'
+
+interface CloudinaryResult {
+	public_id: string
+}
+
+const UploadPage = () => {
+	const [publicId, setPublicId] = useState('')
+
+	return (
+		<>
+			{publicId ? <CldImage src={publicId} width={270} height={180} alt='Image' /> : null}
+			<CldUploadWidget
+				uploadPreset='xb9pwzgr'
+				options={{
+					sources: ['local'],
+					multiple: false,
+					maxFiles: 5,
+				}}
+				onUpload={(result, widget) => {
+					if (result.event !== 'success') return
+					const info = result.info as CloudinaryResult
+					setPublicId(info.public_id)
+				}}
+			>
+				{({ open }) => {
+					return (
+						<button className='btn btn-primary' onClick={() => open()}>
+							Upload an Image
+						</button>
+					)
+				}}
+			</CldUploadWidget>
+		</>
+	)
+}
+
+export default UploadPage
